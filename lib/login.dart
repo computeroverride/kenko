6ff572132ae1457924c35e7a9cc30193e4600244
Nethra <nethravino77@gmail.com>
2025-07-21 15:49:55 +0800

    final test

--- lib/login.dart
+++ lib/login.dart
@@ -25,7 +25,7 @@ class _LoginState extends State<Login> {
             color: Colors.blueGrey,
             child: const Center(
               child: Text(
-                "Nethra",
+                "KENKO",
                 style: TextStyle(
                   fontSize: 24,
                   fontWeight: FontWeight.bold,
